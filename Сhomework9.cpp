9d3cbcd4c3f2d4d81724b5e0799424d569d8743f
Nazar <Nazar@COMPUTER>
2024-03-25 15:20:07 +0200

    Add project files.

---
+++ Сhomework9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int nums(int a, int b)
+{
+	if (b == 0) {
+		return a;
+	}
+	else {
+		return nums(b, a % b);
+	}
+}
+int main()
+{
+	int a, b;
+	cout << "Enter 1 num : ";
+	cin >> a;
+	cout << "Enter 2 num : ";
+	cin >> b;
+	int result = nums(a, b);
+	cout << "common divisor of two integers : " << result << endl;
+}
